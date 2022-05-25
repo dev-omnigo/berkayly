e44616954f82fff07d91dad77bf4ae0031fa1070
dev-omnigo <rony@omnigo.ca>
2022-05-25 11:31:48 -0400

    new commit  again

--- public/js/function.js
+++ public/js/function.js
@@ -2,10 +2,14 @@ const main = () => {
 	const handleHeroHoverShine = () => {
 		const left = document.getElementById("hero-left");
 		const right = document.getElementById("hero-right");
+		const leftCta = document.getElementById("leftCta");
+		const rightCta = document.getElementById("rightCta");
 
 		const handleft = (e) => {
-			console.log("yeah!!!")
 			left.classList.add("shine");
+			setTimeout(() => {
+				leftCta.classList.add("inView");
+			}, 1000);
 			left.removeEventListener("mouseover", handleft);
 		};
 		const handleright = (e) => {
