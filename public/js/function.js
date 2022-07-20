7bc97975825ebf94641e67db4978d3f885d937fa
gederooney <gyebadokpo61@gmail.com>
2022-07-20 16:21:42 -0400

    changes and pellicules solaires page

--- public/js/function.js
+++ public/js/function.js
@@ -23,6 +23,7 @@ $(document).ready(function () {
 	});
 
 	// main
+	// home
 	const sideHeroes = $(".side-hero");
 	$.each(sideHeroes, (i, el) => {
 		$(el).mouseenter((e) => {
@@ -38,5 +39,40 @@ $(document).ready(function () {
 			if ($(el).find(".bg").hasClass("removeGrayScale"))
 				$(el).find(".bg").removeClass("removeGrayScale");
 		});
+	});
+	// pellicules solaires
+	const subnav = $("main[data-content='pellicules-solaires'] .sub-nav li");
+	$.each(subnav, (i, el) => {
+		$(el).click((e) => {
+			e.preventDefault();
+			if (!$(el).hasClass("active")) {
+				$("main[data-content='pellicules-solaires']")
+					.find("li.active")
+					.removeClass("active");
+				$("main[data-content='pellicules-solaires'] .right-body")
+					.find(".active")
+					.removeClass("active");
+				$(
+					`.right-body > div[data-trigger="${$(el).attr(
+						"data-trigger"
+					)}"]`
+				).addClass("active");
+				$(el).toggleClass("active");
+			}
+		});
+	});
+
+	const carouselBox = $(
+		'main[data-content="pellicules-solaires"] .images-carousel-box'
+	);
+	const slides = $(carouselBox).find(".item:not(.hide)");
+	const slidesIndexes = $(slides)
+		.toArray()
+		.map((x, i) => i);
+	$(slides).each((i, s) => $(s).attr("data-slide", slidesIndexes[i]));
+	$('span[data-role="handle-carousel"]').click((e) => {
+		e.preventDefault();
+		slidesIndexes.unshift(slidesIndexes.pop());
+		$(slides).each((i, s) => $(s).attr("data-slide", slidesIndexes[i]));
 	});
 });
